7d6c8b4ebc87d9422e5896bbfeaa59b03cfc85de
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-08-13 17:31:15 +0900

    利用可能なAPIをチェックするためにScopeをコールする処理をMainに実装

--- src/main/java/sendgrid/sandbox/Main.java
+++ src/main/java/sendgrid/sandbox/Main.java
@@ -1,16 +1,34 @@
 package sendgrid.sandbox;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import java.util.Properties;
+import static sendgrid.sandbox.SendGridPropertiesIds.*;
+
 /**
- *
+ * 
  * @author kikuta
  */
 public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
+     * @throws com.mashape.unirest.http.exceptions.UnirestException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, UnirestException {
+        Properties sendGridProp = new Properties();
+        sendGridProp.load(ClassLoader.getSystemResourceAsStream(SG_PROP_FILE_NAME));
         
+        //API Scopeの確認（利用可能なAPIの確認）
+        HttpResponse<JsonNode> result = Unirest.get("https://api.sendgrid.com/v3/scopes")
+                .header("Authorization", "Bearer " + sendGridProp.getProperty(SG_API_KEY))
+                .header("Content-Type", "application/json")
+                .asJson();
+        
+        System.out.println(result.getBody().toString());
     }
-    
 }
---
+++ src/main/java/sendgrid/sandbox/SendGridPropertiesIds.java
@@ -0,0 +1,13 @@
+package sendgrid.sandbox;
+
+/**
+ * 
+ * @author kikuta
+ */
+public class SendGridPropertiesIds {
+    public static final String SG_PROP_FILE_NAME = "sendgrid.properties";
+    public static final String SG_API_KEY = "APIKey";
+    public static final String SG_API_KEY_ID = "APIKeyId";
+    public static final String SG_MAIL_TO = "To";
+    public static final String SG_MAIL_FROM = "From";
+}
