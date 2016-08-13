ebddad2edf46785ac077ca53f8696834f621f652
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-08-13 17:34:11 +0900

    JUnitのRule(ExternalResource)を用いたテスト用オブジェクトで固定記述してたIDを定義済み変数の利用へ修正

--- src/test/java/sendgrid/sandbox/rule/SendGridResource.java
+++ src/test/java/sendgrid/sandbox/rule/SendGridResource.java
@@ -2,6 +2,7 @@ package sendgrid.sandbox.rule;
 
 import java.util.Properties;
 import org.junit.rules.ExternalResource;
+import static sendgrid.sandbox.SendGridPropertiesIds.*;
 
 /**
  * SendGridResource.
@@ -17,14 +18,21 @@ public class SendGridResource extends ExternalResource {
     @Override
     protected void before() throws Throwable {
         sendGridProp = new Properties();
-        sendGridProp.load(this.getClass().getResourceAsStream("/sendgrid.properties"));
+        sendGridProp.load(this.getClass().getResourceAsStream("/" + SG_PROP_FILE_NAME));
     }
     
     public String apiKey() {
         if(sendGridProp == null) {
             System.out.println("sendgrid.properties is not existed.");
         }
-        return sendGridProp.getProperty("APIKeys");
+        return sendGridProp.getProperty(SG_API_KEY);
+    }
+    
+    public String apiKeyId() {
+        if(sendGridProp == null) {
+            System.out.println("sendgrid.properties is not existed.");
+        }
+        return sendGridProp.getProperty(SG_API_KEY_ID);
     }
     
     public String to() {
